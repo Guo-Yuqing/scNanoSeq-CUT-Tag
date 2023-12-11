535ee387ee3adb786872cf28207c02606f3efee0
Guo-Yuqing <153424744+Guo-Yuqing@users.noreply.github.com>
2023-12-11 13:44:36 +0800

    Update sbatch_co_occupancy.sh

--- scNanoSeq-CUT_Tag/06_co_peaks/sbatch_co_occupancy.sh
+++ scNanoSeq-CUT_Tag/06_co_peaks/sbatch_co_occupancy.sh
@@ -9,24 +9,23 @@ for cellline in K562 GM12878 HG002 293T H9 HFF1
 do 
 	for chr_ in chr{1..22} chrX
 	do 
-
-    run_log=$workdir/run_log/$cellline
-    mkdir -p $run_log
-
-    echo "#!/bin/bash
-    sh $scripts  $cellline $antibody $chr_
-    " > $run_log/${cellline}_${chr_}_co_accessibility.sh
-
-    chmod +x $run_log/${cellline}_${chr_}_co_accessibility.sh
-
-    pkubatch \
-        -J GYQ_${cellline}_${antibody}_coa \
-        -p cn-long \
-        -A tangfuchou_g1 \
-        -c 5  \
-        --qos=tangfuchoucnl \
-        -o $run_log/${cellline}_${chr_}_co_accessibility.o -e $run_log/${cellline}_${chr_}_co_accessibility.e \
-        $run_log/${cellline}_${chr_}_co_accessibility.sh
+	    run_log=$workdir/run_log/$cellline
+	    mkdir -p $run_log
+	
+	    echo "#!/bin/bash
+	    sh $scripts  $cellline $antibody $chr_
+	    " > $run_log/${cellline}_${chr_}_co_accessibility.sh
+	
+	    chmod +x $run_log/${cellline}_${chr_}_co_accessibility.sh
+	
+	    pkubatch \
+	        -J GYQ_${cellline}_${antibody}_coa \
+	        -p cn-long \
+	        -A tangfuchou_g1 \
+	        -c 5  \
+	        --qos=tangfuchoucnl \
+	        -o $run_log/${cellline}_${chr_}_co_accessibility.o -e $run_log/${cellline}_${chr_}_co_accessibility.e \
+	        $run_log/${cellline}_${chr_}_co_accessibility.sh
 	done
 done
 
