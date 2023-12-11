7bc21a10345360efd01f38e55d92ed56532379e6
Guo-Yuqing <153424744+Guo-Yuqing@users.noreply.github.com>
2023-12-11 13:27:00 +0800

    Update S02_pkubatch_02_trim_mapping.sh

--- scNanoSeq-CUT_Tag/01_preprocess/S02_pkubatch_02_trim_mapping.sh
+++ scNanoSeq-CUT_Tag/01_preprocess/S02_pkubatch_02_trim_mapping.sh
@@ -5,32 +5,29 @@ cd $workdir
 scripts=$workdir/run_scripts/02_trim_mapping.sh
 cat $workdir/samplelist/$samdir|grep -v "^#"|while read line
 do
- echo "$line">./${date}_temp.txt
- cell=`awk '{print $1}' ./${date}_temp.txt`
- batch=`awk '{print $2}' ./${date}_temp.txt`
- prefix=`awk '{print $3}' ./${date}_temp.txt`
+  echo "$line">./${date}_temp.txt
+  cell=`awk '{print $1}' ./${date}_temp.txt`
+  batch=`awk '{print $2}' ./${date}_temp.txt`
+  prefix=`awk '{print $3}' ./${date}_temp.txt`
+  
+  rm ./${date}_temp.txt
  
- rm ./${date}_temp.txt
-
-run_log=$workdir/run_scripts/run_log/$batch
-mkdir -p $run_log
-
-echo "#!/bin/bash
-sh $scripts  $cell $batch $prefix
-" > $run_log/${cell}_02_trim_mapping.sh
-
-
-chmod +x $run_log/${cell}_02_trim_mapping.sh
-
-pkubatch \
-   -J GYQ_${cell} \
-   -p cn-long \
-   -A tangfuchou_g1 \
-   -c 10  \
-   --qos=tangfuchoucnl \
-   -o $run_log/${cell}_02_trim_mapping.o -e $run_log/${cell}_02_trim_mapping.e \
-   $run_log/${cell}_02_trim_mapping.sh
-
-
+ run_log=$workdir/run_scripts/run_log/$batch
+ mkdir -p $run_log
+ 
+ echo "#!/bin/bash
+ sh $scripts  $cell $batch $prefix
+ " > $run_log/${cell}_02_trim_mapping.sh
+ 
+ chmod +x $run_log/${cell}_02_trim_mapping.sh
+ 
+ pkubatch \
+    -J GYQ_${cell} \
+    -p cn-long \
+    -A tangfuchou_g1 \
+    -c 10  \
+    --qos=tangfuchoucnl \
+    -o $run_log/${cell}_02_trim_mapping.o -e $run_log/${cell}_02_trim_mapping.e \
+    $run_log/${cell}_02_trim_mapping.sh
 
 done
