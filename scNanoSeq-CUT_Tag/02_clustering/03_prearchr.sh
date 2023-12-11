b596d2a8e71448bb1c49ac7370bb24798c43cb2f
Guo-Yuqing <153424744+Guo-Yuqing@users.noreply.github.com>
2023-12-11 13:27:53 +0800

    Update 03_prearchr.sh

--- scNanoSeq-CUT_Tag/02_clustering/03_prearchr.sh
+++ scNanoSeq-CUT_Tag/02_clustering/03_prearchr.sh
@@ -3,14 +3,14 @@ antibody=$1
 flank_size=100
 for cell in GM12878 HG002 K562 293T H9 HFF1 
 do 
-prefix=${cell}_${antibody}
-cd ${cell}
-
-cat ${cell}_*/${cell}_${antibody}_*.rmdup_fragments|grep -v '\.' | \
-awk '{print "chr"$1"\t"$2"\t"$3"\t"$4"\t"$5}'| \
-sort -k 1,1V -k 2,2n -k 3,3n >${prefix}_merge_all_cell.rmdup_fragments.site.bed
-
-cd ../
+  prefix=${cell}_${antibody}
+  cd ${cell}
+  
+  cat ${cell}_*/${cell}_${antibody}_*.rmdup_fragments|grep -v '\.' | \
+  awk '{print "chr"$1"\t"$2"\t"$3"\t"$4"\t"$5}'| \
+  sort -k 1,1V -k 2,2n -k 3,3n >${prefix}_merge_all_cell.rmdup_fragments.site.bed
+  
+  cd ../
 done
 
 mkdir arrow_file  && cd arrow_file
@@ -20,10 +20,10 @@ prefix=${antibody}
 for i in K562 GM12878 293T HFF1 H9 HG002
 do
 
-less ${i}_${prefix}_merge_all_cell.rmdup_fragments.site.bed| \
-awk '{if($2-50>0)print $1"\t"$2-50"\t"$2+50"\t"$4"\t"$5;else print $0}' >${i}_${prefix}_merge_all_cell.rmdup_fragments.site.tmp1
-less ${i}_${prefix}_merge_all_cell.rmdup_fragments.site.bed| \
-awk '{if($3-50>0)print $1"\t"$3-50"\t"$3+50"\t"$4"\t"$5;else print $0}' >${i}_${prefix}_merge_all_cell.rmdup_fragments.site.tmp2
+  less ${i}_${prefix}_merge_all_cell.rmdup_fragments.site.bed| \
+  awk '{if($2-50>0)print $1"\t"$2-50"\t"$2+50"\t"$4"\t"$5;else print $0}' >${i}_${prefix}_merge_all_cell.rmdup_fragments.site.tmp1
+  less ${i}_${prefix}_merge_all_cell.rmdup_fragments.site.bed| \
+  awk '{if($3-50>0)print $1"\t"$3-50"\t"$3+50"\t"$4"\t"$5;else print $0}' >${i}_${prefix}_merge_all_cell.rmdup_fragments.site.tmp2
 
 done
 
