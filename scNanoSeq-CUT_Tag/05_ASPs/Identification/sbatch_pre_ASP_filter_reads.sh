175a446a450ed2c935125b762820db0aa26b827f
Guo-Yuqing <153424744+Guo-Yuqing@users.noreply.github.com>
2023-12-11 13:43:21 +0800

    Update sbatch_pre_ASP_filter_reads.sh

--- scNanoSeq-CUT_Tag/05_ASPs/Identification/sbatch_pre_ASP_filter_reads.sh
+++ scNanoSeq-CUT_Tag/05_ASPs/Identification/sbatch_pre_ASP_filter_reads.sh
@@ -6,29 +6,29 @@ scripts=$workdir/pre_ASP_filter_reads.sh
 cat $workdir/sample.list|grep -v "^#"|while read line
 do
 
- echo "$line">./${date}_temp.txt
- batch=`awk '{print $1}' ./${date}_temp.txt`
- cell=`awk '{print $2}' ./${date}_temp.txt`
-
- rm ./${date}_temp.txt
-
-run_log=$workdir/$batch
-mkdir -p $run_log
-
-echo "#!/bin/bash
-sh $scripts  $batch $cell
-" > $run_log/${cell}_SNP.sh
-
-chmod +x $run_log/${cell}_SNP.sh
-
-pkubatch \
-   -J GYQ_${cell} \
-   -p cn-long \
-   -A tangfuchou_g1 \
-   -c 10  \
-   --qos=tangfuchoucnl \
-   -o $run_log/${cell}_SNP.o -e $run_log/${cell}_SNP.e \
-   $run_log/${cell}_SNP.sh
+   echo "$line">./${date}_temp.txt
+   batch=`awk '{print $1}' ./${date}_temp.txt`
+   cell=`awk '{print $2}' ./${date}_temp.txt`
+  
+   rm ./${date}_temp.txt
+  
+  run_log=$workdir/$batch
+  mkdir -p $run_log
+  
+  echo "#!/bin/bash
+  sh $scripts  $batch $cell
+  " > $run_log/${cell}_SNP.sh
+  
+  chmod +x $run_log/${cell}_SNP.sh
+  
+  pkubatch \
+     -J GYQ_${cell} \
+     -p cn-long \
+     -A tangfuchou_g1 \
+     -c 10  \
+     --qos=tangfuchoucnl \
+     -o $run_log/${cell}_SNP.o -e $run_log/${cell}_SNP.e \
+     $run_log/${cell}_SNP.sh
 
 
 done
